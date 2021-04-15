1889bedddf0486a5edb0073985ce446e864c1a81
Lucas Matos <lucas@bertonisolutions.com>
2021-04-14 19:10:47 -0600

    update style

--- src/com/NavbarComponent.tsx
+++ src/com/NavbarComponent.tsx
@@ -9,13 +9,11 @@ import { colors } from "@material-ui/core";
 
 const StyledAppBar = withStyles({
   root: {
-    backgroundColor: "#333",
     borderRadius: 3,
     border: 0,
     color: "white",
     height: 48,
     padding: "0 30px",
-    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
   },
 })(AppBar);
 
